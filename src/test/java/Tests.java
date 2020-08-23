60aa36bcafc76ff6403519e3eba34944901b1561
chrisimi <mittermaier.c@a1.net>
2020-08-23 12:05:24 +0200

    added first junit test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,6 +1,16 @@
 package test.java;
 
 
+import com.chrisimi.numberformatter.NumberFormatter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Tests
 {
+    @Test
+    public void Test1()
+    {
+        assertEquals("500.0$", NumberFormatter.format(500));
+    }
 }
